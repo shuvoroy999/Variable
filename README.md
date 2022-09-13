53ae4dc9ee0a196dc1682f0313f1ad115dd02367
Shuvo Roy <88674186+shuvoroy999@users.noreply.github.com>
2022-09-13 14:23:08 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Variable
+ js variable
---
+++ variable.js
@@ -0,0 +1,16 @@
+
+//Var variable
+var a = "Shuvo";    //var is a golobae scpoe variable
+var b = " Roy";     //this variable use multi time
+var c = a + b;
+console.log("My name is " + c);
+
+// Let variable
+let d = 5;          //let is a block scpoe variable
+let e = 5;          //this variable use only one time in a block
+let f = d + e;      //never use golovally another time
+console.log("C Value is = " + f);
+
+//Const variable
+const pi = 3.1416;  //const is a static variable, its value cannt change
+console.log(pi);    //this variable use only expactional purpas,like(arry,object,Function,RegExp)
